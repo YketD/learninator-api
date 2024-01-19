a40bcc49d36a3f0bff6d0e9d104ff1694067b6f9
Yke ten Donkelaar <y.tendonkelaar@code14.nl>
2024-01-19 13:03:52 +0100

    #main - add model property to configuration

--- app/Actions/GenerateQuestionAction.php
+++ app/Actions/GenerateQuestionAction.php
@@ -13,10 +13,11 @@ class GenerateQuestionAction
 
     public function execute(User $user, ?int $question_type_id = null, ?bool $fresh = false, ?int $interest_id = null)
     {
+        if (!$question_type_id) {
+            $question_type_id = random_int(1, 3);
+        }
+
         switch ($question_type_id) {
-            case 1:
-                $question = $this->generateWordGuesserQuestion($user, 1, $fresh, $interest_id);
-                break;
             case 2:
                 $question = $this->GenerateDefinitionGuesserQuestion($user, 2, $fresh, $interest_id);
                 break;
@@ -24,7 +25,7 @@ class GenerateQuestionAction
                 $question = $this->generateSwedishPuzzleQuestion($user, 3, $fresh, $interest_id);
                 break;
             default:
-                $question = $this->generateRandomQuestion($user, $fresh);
+                $question = $this->generateWordGuesserQuestion($user, 1, $fresh, $interest_id);
                 break;
         }
 
@@ -61,19 +62,19 @@ class GenerateQuestionAction
 
         if ($interest_id) {
             $interest = Interest::query()->where('id', $interest_id)->first();
-            $prompt = 'Creëer een woordraadvraag voor een interesse in ' . $interest->name;
+            $prompt = 'Creëer een woordraadvraag voor een interesse in ' . $interest->name . ', met als mogelijke antwoorden 4 woorden';
         } elseif ($user->interests->count() > 0) {
             $interest = $user->interests->random();
-            $prompt = 'Creëer een woordraadvraag voor een interesse in  ' . $interest->name;
+            $prompt = 'Creëer een woordraadvraag voor een interesse in  ' . $interest->name . ', met als mogelijke antwoorden 4 woorden';
         }   else {
             $interest = Interest::query()->inRandomOrder()->first();
-            $prompt = 'Creëer een woordraadvraag voor een interesse in ' . $interest->name;
+            $prompt = 'Creëer een woordraadvraag voor een interesse in ' . $interest->name . ', met als mogelijke antwoorden 4 woorden';
         }
 
         Interest::query()->where('id', $interest_id)->first();
 
         $gptResponse = $openai->chat()->create([
-            'model' => 'gpt-3.5-turbo-1106',
+            'model' => config('openai.model'),
             'messages' => [
                 [
                     'role' => 'user',
@@ -153,7 +154,7 @@ class GenerateQuestionAction
         $openai = OpenAI::client(config('openai.api_key'), config('openai.organization'));
 
         $gptResponse = $openai->chat()->create([
-            'model'         => 'gpt-3.5-turbo-1106',
+            'model'         => config('openai.model'),
             'messages'      => [
                 [
                     'role'    => 'user',
@@ -229,21 +230,13 @@ class GenerateQuestionAction
             $interest = Interest::query()->inRandomOrder()->first();
         }
 
-        $prompt = "Genereer alsjeblieft een woord van 8 tot 10 tekens binnen het interessegebied $interest->name.
-        bedenk zelf een korte definitie of hint voor het woord, maximaal 3 woorden. 
-        Het doel is dat de gebruiker het ontbrekende deel van het woord invult op basis van de gegenereerde hint. 
-        Zorg ervoor dat het woord uitdagend is voor het trainen van vocabulaire binnen het specifieke interessegebied.";
-
-        // $prompt = 'Creëer een Zweedse puzzelwoord voor een interesse in ' . $interest->name . '.
-        // De "question" moet het nederlandse woord zijn, met ' . rand(8, 10) . ' letters,
-        // waarvan er maximaal 4 letters zichtbaar mogen zijn,
-        // en de rest vervangen door _.
-        // de "short_definition" moet een hint zijn van de betekenis van het woord & mag het woord niet bevatten.';
+        $prompt = "Genereer een uitdagend $interest->name -woord (7-11 tekens) met een korte hint (max 2 woorden) voor de gebruiker om te raden,
+         zonder het volledige woord te onthullen.";
 
         $openai = OpenAI::client(config('openai.api_key'), config('openai.organization'));
 
         $gptResponse = $openai->chat()->create([
-            'model'         => 'gpt-3.5-turbo-1106',
+            'model'         => config('openai.model'),
             'messages'      => [
                 [
                     'role'    => 'user',
@@ -256,10 +249,10 @@ class GenerateQuestionAction
                     'parameters' => [
                         'type'       => 'object',
                         'properties' => [
-                            'short_definition' => ['type' => 'string'],
+                            'hint' => ['type' => 'string'],
                             'volledig_woord'   => ['type' => 'string'],
                         ],
-                        'required'   => ['volledig_woord', 'short_definition'],
+                        'required'   => ['volledig_woord', 'hint'],
                     ],
                 ],
             ],
@@ -268,12 +261,12 @@ class GenerateQuestionAction
 
         $functionCall = $gptResponse['choices'][0]['message']['function_call'];
         $questionJson = json_decode($functionCall['arguments'], true);
-        Log::info($questionJson);
+
         $question = new Question();
         $question->prompt = $this->verbergLetters($questionJson['volledig_woord']);
         $question->question_type_id = 3;
         $question->interest_id = $interest->id;
-        $question->short_definition = $questionJson['short_definition'];
+        $question->short_definition = $questionJson['hint'];
         $question->save();
 
         $question->options()->create([
@@ -287,17 +280,19 @@ class GenerateQuestionAction
     function verbergLetters($woord) {
         $lengte = strlen($woord);
         $verborgenWoord = '';
+        $minimaalAantalLetters = 2;
+        $vervangenCount = 0;
 
         for ($i = 0; $i < $lengte; $i++) {
-            // Vervang letters door underscores, behalve spaties
-            if ($woord[$i] !== ' ') {
+            // Bepaal willekeurig of een letter wordt vervangen door een underscore, behalve spaties
+            if ($woord[$i] !== ' ' && ($vervangenCount < ($lengte - $minimaalAantalLetters)) && rand(0, 2)) {
                 $verborgenWoord .= '_';
+                $vervangenCount++;
             } else {
-                $verborgenWoord .= ' ';
+                $verborgenWoord .= $woord[$i];
             }
         }
 
         return $verborgenWoord;
     }
-
 }
