249ed1540f59254f25678c49924fa408f14a9511
Yke ten Donkelaar <y.tendonkelaar@code14.nl>
2024-01-23 19:45:43 +0100

    #main - fix request input

--- app/Actions/LoadQuestionsAction.php
+++ app/Actions/LoadQuestionsAction.php
@@ -10,18 +10,18 @@ class LoadQuestionsAction
     public function execute(QuestionRequest $request)
     {
         return Question::query()
-            ->when($request->input('question_type_id'), function ($query, $question_type_id) {
-                $query->where('question_type_id', $question_type_id);
+            ->when($request->input('question_type_id'), function ($query) use ($request) {
+                $query->where('question_type_id', $request->input('question_type_id'));
             })
-            ->when(!$request->input('retry', true), function ($query, $fresh) {
+            ->when(!$request->input('retry', true), function ($query) {
                 $query->where(function ($query) {
                     $query->whereDoesntHave('answers')
                         ->orWhereHas('answers', function ($query) {
                             $query->whereHas('options.is_correct', false);
                         });
                 });
-            })->when($request->input('interest_id'), function ($query, $interest_id) {
-                $query->where('interest_id', $interest_id);
+            })->when($request->input('interest_id'), function ($query) use ($request) {
+                $query->where('interest_id', $request->input('interest_id'));
             })
             ->with(['options', 'interest'])
             ->paginate($request->input('per_page', 10), ['*'], 'page', $request->input('page', 1));
